703003c7a5c77c2911de6be5f17ead72df9e3408
Erkki Rasi <erkkirasi@gmail.com>
2022-06-07 13:28:45 +0300

    initial commit

---
+++ ERDiary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ERDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+        static bool MainMenu()
+        {
+            Console.Write("Kirjoita:" +
+                "\n1) Syöttääksesi uusi aihe"+
+                "\n2) Tulostaaksesi kaikki aiheet " +
+                "\ntai paina ENTER lopettaaksesi. ");
+            string input = Console.ReadLine();
+                
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            else if (input == "1")
+            {
+                Console.Write("Syötä aihe: ");
+                string title = Console.ReadLine();
+                Topic t = new Topic(title)
+            }
+
+            else if (input == "2")
+            { 
+                Topic.PrintAllTopics(); 
+            }
+                
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Kokeile uudestaan.");
+            }
+            return true;
+            
+        }
+
+
+    }
+
+}
---
+++ ERDiary/Topic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ERDiary
+{
+    /*
+     Id – int - Tähän talletetaan kyseisen aiheen tunniste
+Title – string - Aiheen otsikko
+Description – string - Aiheen kuvaus
+EstimatedTimeToMaster  - double - Aika-arvio, kuinka kauan aiheen oppimiseen kuluu aikaa
+TimeSpent - double - Käytetty aika
+Source – string - Mahdollinen lähde esim web url tai kirja
+StartLearningDate – datetime - Aloitusaika
+InProgress – bool - Onko aiheen opiskelu kesken
+CompletionDate - datetime - Milloin aihe on opiskeltu*/
+    public class Topic
+    { 
+        private int Id { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private double EstimatedTimeToMaster { get; set; }
+        private double TimeSpent { get; set; }
+        private string Source { get; set; }
+        private DateTime StartLearningDate { get; set; }
+        private bool InProgress { get; set; }
+        private DateTime CompletionDate { get; set; }
+        public static List<Topic> topics = new List<Topic>();
+        //private static int counter = 1;
+        public Topic(string title)
+        {
+            //Id = counter;
+            //counter++;
+            Title = title;
+            Id = topics.Count + 1;
+            topics.Add(this);
+            Console.WriteLine("Lisäsit aiheen " + Title + ", jonka id on "+Id+" \n");
+        }
+
+        //public static void AddNewTopic(string title)
+        //{
+        //    topics.Add(new Topic(title));
+        //}
+
+        public static void PrintAllTopics()
+        {
+            Console.Clear();
+            Console.WriteLine("Kaikki syötetyt aiheet:");
+            foreach (var topic in topics)
+            {
+                Console.WriteLine(topic.Title);
+            }
+            Console.WriteLine();
+        }
+        
+    }
+
+}
